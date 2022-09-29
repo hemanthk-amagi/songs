93783b1bab33100243abf18c6d3097501269f023
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-29 16:33:31 +0530

    conditional rendering

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = (props) => {
-  return <div>Song Detail</div>
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>
+  }
+  return (
+    <div>
+      <h3>Details for: </h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Song Duration: {song.duration}
+      </p>
+    </div>
+  )
 }
 
 
 const mapStateToProps = (state) => {
-  console.log(state)
   return { song: state.selectedSong }
 }
 export default connect(mapStateToProps)(SongDetail);
