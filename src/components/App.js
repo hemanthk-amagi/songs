bd3ad449d2bba4214d4dc6bf9ae14ec7284a6b0b
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-29 13:55:29 +0530

    The connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { selectSong } from "../actions";
+import SongList from "./SongList";
 
 const App = () => {
-  return <div>Hi</div>
+  return (
+    <div>
+      <SongList />
+    </div>
+  )
 }
 export default App;
