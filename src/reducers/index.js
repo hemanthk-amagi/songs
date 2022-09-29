f3fc53fa54132fca12077b9b7b8f608102a1b635
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-29 13:44:34 +0530

    building reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songReducer = () => {
+  return [
+    { title: 'ye dosti', duration: '4:05' },
+    { title: 'feeling good', duration: '2:30' },
+    { title: 'all star', duration: '3:15' },
+    { title: 'everthing is good', duration: '1:45' },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload
+  }
+  return selectedSong
+}
